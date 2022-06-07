8bb32c4a55bc5d5a97a2dc3529d1905d5fa7a220
Johannes <johannesr@viken.no>
2022-06-07 15:07:20 +0200

    added verbosity

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from math import floor
 import os
 
 
@@ -22,18 +23,22 @@ def find_images(src_dir:str) -> list:
 
 # takes array of file names
 # this can be procedurally generated as above or done through
-def convert(images:list):
+def convert(images:list, verbose=True):
+    N = len(images)
+    index = 0
     for i in images:
+        index+=1
         try:
             im = Image.open(SOURCE_IMAGE_PATH+i+".ppm")
             try:
                 im.save(DEST_IMAGE_PATH+i+".png")
+                if verbose:
+                    print(f"\r--------{floor((index/N)*100)}%--------", end="")
+                    continue
             except: 
-                print('failed to save' + i)
+                print('failed to save ' + i)
         except:
             print("failed converting: "+i)
-
-
 
 
 if __name__ == "__main__":
